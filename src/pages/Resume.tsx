f4851e2ea0ec684dc1f6315bab41b7cfddfe746c
Evelas <evelas94@gmail.com>
2021-05-08 11:58:10 +0300

    css

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -8,7 +8,7 @@ const Resume: React.FC = () => {
         <div className="main__info">
           <h1>Resume</h1>
         </div>
-        <iframe className="resume__frame" title="resume" src="https://vk.com/doc375446290_600553039?hash=e8919205c8e5aea88b&dl=8a6f897b5dc01fd569" ></iframe>
+        <iframe className="resume__frame" title="resume" src="https://volspb.ru/play/Shuvalov.pdf" ></iframe>
       </div>
     </div>
   );
